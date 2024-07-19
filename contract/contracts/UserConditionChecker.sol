4bed0e43895dc11bfc626f0988743b9b9a2935d2
Kamal Nayan <geniusamansingh@gmail.com>
2024-07-19 07:11:26 +0000

    condition checker for defi protocols

---
+++ contract/contracts/UserConditionChecker.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity >=0.8.13 <0.9.0;
+
+import "fhevm/lib/TFHE.sol";
+import "./UserDataStorage.sol";
+
+contract UserConditionChecker {
+    UserDataStorage private dataStorage;
+
+    constructor(address _dataStorage) {
+        dataStorage = UserDataStorage(_dataStorage);
+    }
+
+    function isUserAgeAbove18(address user) external view returns (bool) {
+        UserDataStorage.EncryptedData memory data = dataStorage.getUserData(user);
+        ebool isAbove18Encrypted = TFHE.gt(data.age, TFHE.asEuint16(18));
+        return TFHE.decrypt(isAbove18Encrypted);
+    }
+
+    function isUserDefiPropertyAbove10000(address user) external view returns (bool) {
+        UserDataStorage.EncryptedData memory data = dataStorage.getUserData(user);
+        ebool isAbove10000Encrypted = TFHE.gt(data.defiProperty, TFHE.asEuint256(10000));
+        return TFHE.decrypt(isAbove10000Encrypted);
+    }
+
+    function isUserAgeAndDefiPropertyAbove(address user, uint16 ageThreshold, uint256 defiPropertyThreshold) 
+        external 
+        view 
+        returns (bool, bool) 
+    {
+        UserDataStorage.EncryptedData memory data = dataStorage.getUserData(user);
+        ebool isAgeAboveEncrypted = TFHE.gt(data.age, TFHE.asEuint16(ageThreshold));
+        ebool isDefiPropertyAboveEncrypted = TFHE.gt(data.defiProperty, TFHE.asEuint256(defiPropertyThreshold));
+        return (TFHE.decrypt(isAgeAboveEncrypted), TFHE.decrypt(isDefiPropertyAboveEncrypted));
+    }
+}
---
+++ contract/contracts/UserDataStorage.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: BSD-3-Clause-Clear
+pragma solidity >=0.8.13 <0.9.0;
+
+import "fhevm/abstracts/EIP712WithModifier.sol";
+import "fhevm/lib/TFHE.sol";
+
+contract UserDataStorage is EIP712WithModifier {
+    address public trustedDataProvider;
+
+    struct EncryptedData {
+        euint16 age;
+        euint256 defiProperty;
+    }
+
+    mapping(address => EncryptedData) internal userData;
+
+    constructor() EIP712WithModifier("User Data Authorization", "1") {
+        trustedDataProvider = msg.sender;
+    }
+
+    modifier onlyProvider {
+        require(msg.sender == trustedDataProvider, "Caller is not the trusted provider");
+        _;
+    }
+
+    function storeUserData(address user, bytes calldata encryptedAge, bytes calldata encryptedDefiProperty) 
+        external 
+        onlyProvider 
+    {
+        userData[user] = EncryptedData({
+            age: TFHE.asEuint16(encryptedAge),
+            defiProperty: TFHE.asEuint256(encryptedDefiProperty)
+        });
+    }
+
+    function getUserData(address user) external view returns (EncryptedData memory) {
+        return userData[user];
+    }
+}
